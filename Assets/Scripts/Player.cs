6deb9aae226dc207f443d7534a869f61e006eec8
Monky216 <90779085+Monky216@users.noreply.github.com>
2023-02-08 11:17:12 -0500

    Trying to prevent going up or down
    No luck on anything, might need a completely different system of movement for that to work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float xRange;
+    public float yRange;
     public float speed;
 
     void Update()
     {
+        //inputs for movement
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
 
+        //setting both as a vector 2
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
+        //calculating the change
         transform.Translate(movement * speed * Time.deltaTime);
+
+        //change so y does not change
+        ///float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        ///transform.position = new Vector2(moveHorizontal * speed * Time.deltaTime, yRange);
+        ///Debug.Log(transform.position);
     }
 
     void LateUpdate()
